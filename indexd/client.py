67f3967b054fe70b756e84aa5b3951f2b957ccca
lilydjwg <lilydjwg@gmail.com>
2012-07-13 10:13:31 +0800

    add a simple client module for testing purpose

---
+++ indexd/client.py
@@ -0,0 +1,34 @@
+# vim:fileencoding=utf-8:sw=4
+
+import socket
+import json
+from pprint import pprint
+
+from util import read_response, write_response
+
+def decode_reply(re):
+  return json.loads(re, encoding='utf-8')
+
+def print_reply(re):
+  if isinstance(re, str):
+    re = decode_reply(re)
+  pprint(re)
+
+class Client(object):
+  def __init__(self, addr):
+    sock = self.sock = socket.socket()
+    sock.connect(addr)
+    sock.send('AWIP/01 RDONLY\r\n')
+    re = sock.recv(1024)
+    print re
+    self.fp = sock.makefile()
+
+  def ping(self):
+    self.do_cmd('ping')
+
+  def do_cmd(self, cmd, **other):
+    d = { 'cmd': cmd }
+    d.update(other)
+    write_response(self.fp, d)
+    print_reply(read_response(self.fp))
+
