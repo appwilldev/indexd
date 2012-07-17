b1745a81d363c6db50b4a7c530ae154a1ecc78c6
lilydjwg <lilydjwg@gmail.com>
2012-07-17 14:13:03 +0800

    more detailed test for inserting

--- tests/test_5_modify.py
+++ tests/test_5_modify.py
@@ -4,6 +4,12 @@ import os
 import unittest
 from base import TestBase, dbdir, config
 
+doc = {
+    'id_NUMBER': 8237,
+    'TITLE': 'test hehe',
+    'DESCRIPTION': 'This is just a document for testing purpose :-)',
+}
+
 class TestCreateInsert(TestBase):
     mode = 'RDWR'
     dbname = 'test_insert'
@@ -17,17 +23,15 @@ class TestCreateInsert(TestBase):
         self.assertMessageFind(ans, 'missing key')
 
     def test_add_ok(self):
-        ans = self.client.insert({
-            'id_NUMBER': 8237,
-            'TITLE': 'test hehe',
-            'DESCRIPTION': 'This is just a document for testing purpose :-)',
-        })
+        ans = self.client.insert(doc)
         self.assertEqual(ans, {u'status': u'ok'})
 
     def test_querydb(self):
         self.test_add_ok()
         ans = self.client.query('test')
-        self.assertTrue(ans['results'])
+        self.assertEqual(ans['results'], [1])
+        ans = self.client.retrieve([1])['results'][0]
+        self.assertEqual(ans, doc)
 
     def tearDown(self):
         os.system("rm -rf '%s'" % os.path.join(dbdir, self.dbname))
