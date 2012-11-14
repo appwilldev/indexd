0b2da74d5a5a38f39c6f8ac93b81d1603dc9158f
lilydjwg <lilydjwg@gmail.com>
2012-11-14 14:14:41 +0800

    no need to test setdb ok; it causes trouble only

--- tests/test_2_dbselect.py
+++ tests/test_2_dbselect.py
@@ -4,10 +4,6 @@ import unittest
 from base import TestBase
 
 class TestDB(TestBase):
-    def test_setok(self):
-        ans = self.client.setdb('db')
-        self.assertEqual(ans['status'], u'ok', 'Got %r' % ans)
-
     def test_setbad(self):
         ans = self.client.setdb('db_nonexistent')
         self.assertMessageFind(ans, 'no such file or directory')
